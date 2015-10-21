f7297a69aed55eecfe3e85c4bc3267822bf27c93
Cina Mehrvar <cinamehrvar@gmail.com>
2015-10-21 10:25:45 -0400

    Initial Commit

---
+++ Lecture3/Lecture3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lecture3
+//
+//  Created by Cina Mehrvar on 2015-10-21.
+//  Copyright © 2015 DormRoom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
